8bc1e13c5d6f19abf71455a69ff5fd06b48c9764
User <user@MacBook-Pro-de-User.local>
2025-10-05 20:40:56 +0200

    Premier commit du projet

--- app.js
+++ app.js
@@ -70,3 +70,13 @@ function handleIntersect(entries) {
     }
   })
 }
+
+// fonction toggleMenu - à faire
+const menuResponsive = document.querySelector('.toggle-menu');
+const iconeToggle = document.querySelector('.icone-toggle');
+
+iconeToggle.addEventListener('click', () => {
+
+    menuResponsive.classList.toggle('hidden');
+
+})
